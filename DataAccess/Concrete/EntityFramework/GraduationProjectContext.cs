9e9f7057c5b8cd8e2698ab0d0f0309b5dd896361
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2022-02-08 18:54:16 +0300

    Added Entities Dal Classes to DataAccess Layer and Context Class

---
+++ DataAccess/Concrete/EntityFramework/GraduationProjectContext.cs
@@ -0,0 +1,28 @@
+﻿using Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    // Veritabanı'ndaki tablolar ile entity'lerimizi ilişkilendirdik.
+
+    public class GraduationProjectContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<InstallmentOption> InstallmentOptions { get; set; }
+        public DbSet<InsuredPerson> InsuredPersons { get; set; }
+        public DbSet<InsuredPersonRelationship> InsuredPersonRelationships { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<PaymentMethod> PaymentMethods { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=LAPTOP-TAKPAA66;Database=GraduationProject;Trusted_Connection=true");
+        }
+    }
+}
