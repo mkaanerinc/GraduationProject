edd19aaac4b6a3fbdc3955993dba430dfaa657e8
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2022-02-09 14:13:20 +0300

    Added FluentValidation and BusinessRules for Business  to Core Layer

---
+++ Core/CrossCuttingConcerns/Validation/FluentValidation/ValidationTool.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Validation.FluentValidation
+{
+    // Fluent Validation için kod yazılımı her validation işlemi için 3 satırdan  tek satıra
+    // indirmek için yazılan static class.
+
+    public static class ValidationTool
+    {
+        public static void Validate(IValidator validator, object entity)
+        {
+            var context = new ValidationContext<object>(entity);
+            var result = validator.Validate(context);
+
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+
+    }
+}
---
+++ Core/Utilities/Business/BusinessRules.cs
@@ -0,0 +1,34 @@
+﻿using Core.Utilities.Results.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Business
+{
+    // Burada Business katmanı için bir manager'da gereken iş kodları sonucu
+    // oluşacak iç içe if kontrolleri manager içine yazmak yerine static
+    // class ile kod yazımını kısalttık.
+    // Her projede kullanılabileceği için Core katmanına eklendi.
+
+    // Çalışma prensibi => IResult dönen iş kodlarını array ile alıp foreach içerisinde
+    // her birinin kontrolünü gerçekleştirirken o iş için false dönerse
+    // İş içindeki ErrorResult döndürülüyor. Diğer türlü ise null.
+
+    public class BusinessRules
+    {
+        public static IResult Run(params IResult[] logics)
+        {
+            foreach (var logic in logics)
+            {
+                if (!logic.Success)
+                {
+                    return logic;
+                }
+            }
+
+            return null;
+        }
+    }
+}
